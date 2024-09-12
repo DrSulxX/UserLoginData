44fc7b8bd833d2ff7f05e382030be64d34a9b35f
Otto Sulaoja <sulaoja@gmail.com>
2024-09-12 15:12:25 +0300

    Delete src/main directory

--- src/main/java/ottosulaoja/drsulxx/repository/backup/BackupUserSecurityRepository.java
+++
@@ -1,7 +0,0 @@
-package ottosulaoja.drsulxx.repository.backup;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ottosulaoja.drsulxx.model.security.UserSecurity;
-
-public interface BackupUserSecurityRepository extends JpaRepository<UserSecurity, Long> {
-}

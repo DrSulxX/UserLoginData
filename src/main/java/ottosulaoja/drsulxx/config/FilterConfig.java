0ec531d09802c8fc82321e745710e0ec3e498232
Otto Sulaoja <sulaoja@gmail.com>
2024-12-20 13:25:49 +0200

    Delete src/main/java/ottosulaoja/drsulxx/config directory

--- src/main/java/ottosulaoja/drsulxx/config/FilterConfig.java
+++
@@ -1,9 +0,0 @@
-package ottosulaoja.drsulxx.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-    // No need to explicitly define the LoggingFilter bean here
-    // Spring Boot will automatically detect and register it due to the @Component annotation in LoggingFilter
-}
